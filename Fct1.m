8f348649853c97560402b1f524016aee9483b218
loiclemarrec <loic.lemarrec@univ-rennes.fr>
2025-09-26 10:46:12 +0200

    modif fct1

--- Fct1.m
+++ Fct1.m
@@ -1,1 +1,8 @@
 %% Fonction 1
+% Geometrie : section cicrculaire
+L=1;            % Longueur [m]
+R=0.001;        % Rayon [m]
+A=pi*R^2;       % Aire [m^2]
+% Materiau : acier
+E=210e9;        % Module de Young [Pa]
+ro=7800;        % Masse volumique [kg/m^3]
